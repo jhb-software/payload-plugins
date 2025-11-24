be0eb7a261069936121b3e33fba6e332e1b718e6
Jens Becker <info@jhb.software>
2025-11-24 14:28:20 +0100

    fix(pages): generate virtual fields for findVersion operations

--- pages/src/hooks/selectDependentFieldsBeforeOperation.ts
+++ pages/src/hooks/selectDependentFieldsBeforeOperation.ts
@@ -2,11 +2,13 @@ import { CollectionBeforeOperationHook } from 'payload'
 import { asPageCollectionConfigOrThrow } from '../utils/pageCollectionConfigHelpers.js'
 import { dependentFields } from './setVirtualFields.js'
 import { getSelectMode } from 'payload/shared'
+import { hasVirtualFieldSelected } from '../utils/hasVirtualFieldSelected.js'
 
 /**
- * A CollectionBeforeOperationHook that alters the select in case a virtual field is selected
- * to ensure that the fields the setVirtualFields hook depends on to correctly generate
- * the virtual fields are also selected.
+ * A CollectionBeforeOperationHook that alters the select in case a virtual field is selected.
+ *
+ * This is ensures that all fields that are required to generate the virtual fields are selected.
+ * Also passes the select to the context to make it available to the setVirtualFields (afterRead) hook.
  */
 export const selectDependentFieldsBeforeOperation: CollectionBeforeOperationHook = async ({
   args,
@@ -17,51 +19,30 @@ export const selectDependentFieldsBeforeOperation: CollectionBeforeOperationHook
     const pageConfig = asPageCollectionConfigOrThrow(args.collection.config)
     const selectMode = getSelectMode(args.select)
     const dependendSelectedFields = dependentFields(pageConfig)
+    const hasVirtualFieldsSelected = hasVirtualFieldSelected(args.select)
 
-    if (selectMode === 'include') {
-      const selectedFields = Object.keys(args.select)
-
-      const virtualFields = ['path', 'breadcrumbs', 'meta']
-      const hasVirtualFieldsSelected = virtualFields.some((field) => selectedFields.includes(field))
-
-      if (hasVirtualFieldsSelected) {
-        // extend the select with the required fields
-        args.select = {
-          ...args.select,
-          ...dependendSelectedFields.reduce((acc, field) => ({ ...acc, [field]: true }), {}),
-        }
-
-        // Indicate that the virtual fields should be generated in the setVirtualFields hook
-        context.generateVirtualFields = true
+    if (hasVirtualFieldsSelected && selectMode === 'include') {
+      // extend the select with the required fields
+      args.select = {
+        ...args.select,
+        ...dependendSelectedFields.reduce((acc, field) => ({ ...acc, [field]: true }), {}),
       }
-    } else if (selectMode === 'exclude') {
-      const selectedFields = Object.keys(args.select)
-      const virtualFields = ['path', 'breadcrumbs', 'meta']
-      const allVirtualFieldsDeselected = virtualFields.every((field) =>
-        selectedFields.includes(field),
+    } else if (hasVirtualFieldsSelected && selectMode === 'exclude') {
+      // min one of the virtual fields needs to be generated
+      // -> remove deselection of the required fields
+      args.select = Object.fromEntries(
+        Object.entries(args.select).filter(([field]) => !dependendSelectedFields.includes(field)),
       )
 
-      if (!allVirtualFieldsDeselected) {
-        // min one of the virtual fields needs to be generated
-        // -> remove deselection of the required fields
-        args.select = Object.fromEntries(
-          Object.entries(args.select).filter(([field]) => !dependendSelectedFields.includes(field)),
-        )
-
-        // if select is empty now, set it to undefined, because an empty select would select nothing
-        if (Object.keys(args.select).length === 0) {
-          args.select = undefined
-        }
-
-        // Indicate that the virtual fields should be generated in the setVirtualFields hook
-        context.generateVirtualFields = true
+      // if select is empty now, set it to undefined, because an empty select would select nothing
+      if (Object.keys(args.select).length === 0) {
+        args.select = undefined
       }
     }
-  } else if (operation == 'read' && !args.select) {
-    // Indicate that the virtual fields should be generated in the setVirtualFields hook
-    // if no select is provided
-    context.generateVirtualFields = true
   }
+
+  // Make the select available to the setVirtualFields (afterRead) hook
+  context.select = args.select
 
   return args
 }
