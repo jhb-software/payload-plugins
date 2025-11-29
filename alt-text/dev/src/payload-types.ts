5af15c4907da966044d9db089702608d16f4ae25
S R Jay <srjaykikani@gmail.com>
2025-11-30 04:05:30 +0530

    chore(alt-text): upgrade dependencies to latest versions

--- alt-text/dev/src/payload-types.ts
+++ alt-text/dev/src/payload-types.ts
@@ -88,6 +88,7 @@ export interface Config {
   db: {
     defaultIDType: string;
   };
+  fallbackLocale: ('false' | 'none' | 'null') | false | null | ('en' | 'de') | ('en' | 'de')[];
   globals: {};
   globalsSelect: {};
   locale: 'en' | 'de';
