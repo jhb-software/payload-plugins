abcbc7c773738c968f5cae05c372752469512935
S R Jay <srjaykikani@gmail.com>
2026-01-23 15:24:02 +0530

    refactor(*): use payload built-in translation system (#34)

--- pages/src/components/client/IsRootPageStatus.tsx
+++ pages/src/components/client/IsRootPageStatus.tsx
@@ -1,10 +1,14 @@
 'use client'
+
 import type { CheckboxFieldClientProps } from '@payloadcms/ui/fields/Checkbox'
 
-import { CheckboxField, useField } from '@payloadcms/ui'
+import { CheckboxField, useField, useTranslation } from '@payloadcms/ui'
 
 import { HomeIcon } from '../../icons/HomeIcon.js'
-import { usePluginTranslation } from '../../utils/usePluginTranslations.js'
+import type {
+  PluginPagesTranslations,
+  PluginPagesTranslationKeys,
+} from '../../translations/index.js'
 
 /**
  * Field which displays either a checkbox to set the page to be root page or a message if the page is the root page.
@@ -14,7 +18,7 @@ export const IsRootPageStatus: React.FC<
 > = ({ field, hasRootPage, path, readOnly }) => {
   const { value } = useField<boolean>({ path })
   const isRootPage = value ?? false
-  const { t } = usePluginTranslation()
+  const { t } = useTranslation<PluginPagesTranslations, PluginPagesTranslationKeys>()
 
   if (isRootPage) {
     return (
@@ -22,7 +26,7 @@ export const IsRootPageStatus: React.FC<
         <div style={{ marginRight: '0.5rem', verticalAlign: 'text-bottom' }}>
           <HomeIcon />
         </div>
-        {t('rootPage')}
+        {t('@jhb.software/payload-pages-plugin:rootPage')}
       </div>
     )
   } else if (!hasRootPage && !isRootPage) {
