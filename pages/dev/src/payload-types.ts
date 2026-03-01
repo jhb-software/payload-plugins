2763b30a0973301f36f5c8038c5fdd3bfddffa57
Jens Becker <info@jhb.software>
2026-03-01 18:43:12 +0100

    chore: upgrade all plugins to latest payload v3.78.0

--- pages/dev/src/payload-types.ts
+++ pages/dev/src/payload-types.ts
@@ -112,6 +112,9 @@ export interface Config {
   globals: {};
   globalsSelect: {};
   locale: 'de' | 'en';
+  widgets: {
+    collections: CollectionsWidget;
+  };
   user: User;
   jobs: {
     tasks: unknown;
@@ -605,6 +608,16 @@ export interface PayloadMigrationsSelect<T extends boolean = true> {
   batch?: T;
   updatedAt?: T;
   createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
